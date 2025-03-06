eed018350a071d6e88ea968a2d65dec6b1d7dee9
Teekon789 <teeboy789456@gmail.com>
2025-03-06 12:41:00 +0700

    test_3.9

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,7 +10,7 @@ module.exports = {
     './components/**/*.{js,ts,jsx,tsx}',
     './styles/**/*.{css,module.css}',
     './*.{js,ts,jsx,tsx}',
-    './node_modules/@heroui/theme/dist/**/*.{js,ts,jsx,tsx}',
+   
   ],
   theme: {
     extend: {
@@ -57,6 +57,6 @@ module.exports = {
   plugins: [
     require('@tailwindcss/forms'), // Plugin for form elements styling
     require('@tailwindcss/typography'), // Plugin for typography
-    heroui(), // Custom plugin for @heroui/react
+    
   ],
 };
